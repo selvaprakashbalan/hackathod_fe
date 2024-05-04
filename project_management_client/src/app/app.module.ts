74b51842f245ac369fad7aafacb8f64ffee9b3fd
Selvaprakash <selvaprakash.balan@finstein.ai>
2024-05-04 18:36:14 +0530

    login page done

--- project_management_client/src/app/app.module.ts
+++ project_management_client/src/app/app.module.ts
@@ -3,14 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ project_management_client/src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule
+  ],
+  exports: [
+    CommonModule,
+    InputTextModule,
+    PasswordModule,
+    ButtonModule
+  ]
+})
+export class SharedModule { }
